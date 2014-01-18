894535b9f1c97dc68a32dc12a71f15a3a5ef56e7
Sukant Garg <garg_sms@yahoo.in>
2014-01-18 19:32:17 +0530

    Basic Structure
    Added index, js, css and pictures that govern the layout.

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en-GB">
+	<head>
+		<meta charset="utf-8" />
+		<link href="css/style.css" rel="stylesheet" />
+		<title>ACM NIT Surat Chapter | Home</title>
+	</head>
+	<body>
+		
+	</body>
+</html>
