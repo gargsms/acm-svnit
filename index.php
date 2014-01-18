9addfb38f8bb280a3c8d6e8d78df82f1d54f0620
Sukant Garg <garg_sms@yahoo.in>
2014-01-19 01:40:06 +0530

    Basic homepage structure.
    For more details, see the images in the layout_pngs folder.

--- index.php
+++ index.php
@@ -9,6 +9,34 @@
 		<title>ACM NIT Surat Chapter | Home</title>
 	</head>
 	<body>
-		
+		<div id="theContainer">
+			<div id="headerBlock">
+				<div id="logoAndThings">
+					<span id="logoSVNIT"></span>
+					<span id="logoText"></span>
+					<span id="logoChapter"></span>
+				</div>
+				<div id="navigationPanel">
+					<span>Home</span>
+					<span>Events</span><!-- place event archives here as well -->
+					<span>Team</span>
+					<span>About</span>
+					<span>Contact</span>
+				</div>
+			</div>
+			<div id="theContent">
+			</div>
+			<div id="theFooter">
+				<span id="copyright"></span>
+				<div id="plugins">
+					<span>Facebook</span>
+					<span>Twitter</span>
+					<span>Google+</span>
+				</div>
+			</div>
+		</div>
+		<noscript style="position: absolute;top: 0;height: auto; width: 100%;background: #f00;text-align: center;color: #fff">This site uses JavaScript for its normal functioning. You may experience trouble navigating at this page. Open the lite version <a href="http://nitsurat.acm.org/lite/">here</a>.
+		</noscript>
+		<script src="js/main.js"></script>
 	</body>
 </html>
