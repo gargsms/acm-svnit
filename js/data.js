7f21bcd59513f6524f0b391f886ddcdab5352bb8
Sukant Garg <garg_sms@yahoo.in>
2014-01-21 17:54:13 +0530

    Modified data.js

--- js/data.js
+++ js/data.js
@@ -16,7 +16,18 @@ var data = {
 			1 : 'Past Events'
 		},
 		contents : {
-			0 : ''
+			0 : '<div class="heading">Event 1\
+			<div class="content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu lectus dolor. Phasellus bibendum adipiscing mi, ac accumsan sem. Nullam convallis tortor id magna pellentesque.</div>\
+			</div>\
+			<div class="heading">Event 2</div>\
+			<div class="content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu lectus dolor. Phasellus bibendum adipiscing mi, ac accumsan sem. Nullam convallis tortor id magna pellentesque.</div>\
+			',
+			1 : '<div class="heading">Event 1\
+			<div class="content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu lectus dolor. Phasellus bibendum adipiscing mi, ac accumsan sem. Nullam convallis tortor id magna pellentesque.</div>\
+			</div>\
+			<div class="heading">Event 2</div>\
+			<div class="content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu lectus dolor. Phasellus bibendum adipiscing mi, ac accumsan sem. Nullam convallis tortor id magna pellentesque.</div>\
+			'
 		}
 	},
 	team : {
@@ -26,7 +37,9 @@ var data = {
 			1 : 'Past Teams'
 		},
 		contents : {
-			0 : ''
+			0 : '',		//make table
+			1 : ''		//make another table
+						// I wish 'make' worked here. :( Not even sudo make
 		}
 	},
 	about : {
@@ -35,7 +48,7 @@ var data = {
 			0 : 'About us'
 		},
 		contents : {
-			0 : ''
+			0 : '<div class="heading"></div>'
 		}
 	},
 	contact : {
